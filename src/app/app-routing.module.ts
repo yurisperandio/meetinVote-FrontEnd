c0d259897a3776ca1c14f5c52177b187e4db78d5
Yuri Sperandio <yuri.sperandio@gmail.com>
2021-11-21 23:03:42 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonFormComponent } from '../app/person/person-form/person-form.component';
+import { PersonListComponent } from '../app/person/person-list/person-list.component';
+import { EditItemComponent } from './item/edit-item/edit-item.component';
+import { ItemFormComponent } from './item/item-form/item-form.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
+import { EditPersonComponent } from './person/edit-person/edit-person.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'persons', component: PersonListComponent },
+  { path: 'addPerson', component: PersonFormComponent },
+  { path: 'edit/:id', component: EditPersonComponent },
+  { path: 'editItem/:id', component: EditItemComponent },
+  { path: 'items', component: ItemListComponent },
+  { path: 'addItem', component: ItemFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonListComponent } from '../app/person/person-list/person-list.component';
+import { PersonService } from './service/person.service';
+import { PersonFormComponent } from '../app/person/person-form/person-form.component';
+import { EditPersonComponent } from './person/edit-person/edit-person.component';
+import { ItemService } from './service/item.service';
+import { ItemFormComponent } from './item/item-form/item-form.component';
+import { ItemListComponent } from '../app/item/item-list/item-list.component';
+import { EditItemComponent } from './item/edit-item/edit-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonListComponent,
+    PersonFormComponent,
+    EditPersonComponent,
+    ItemFormComponent,
+    ItemListComponent,
+    EditItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [PersonService, ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
